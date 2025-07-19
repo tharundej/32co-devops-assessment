31585b75408008ccfc49dd415106c87b35fed1d5
tharundej <95700240+tharundej@users.noreply.github.com>
2025-07-19 23:53:20 +0530

    Update variables.tf

--- infrastructure/modules/ec2/variables.tf
+++ infrastructure/modules/ec2/variables.tf
@@ -67,3 +67,78 @@ variable "aws_account_id" {
   description = "AWS Account ID used in constructing resource ARNs"
   type        = string
 }
+
+variable "app_name" {
+  description = "Application name prefix"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for ASG"
+  type        = list(string)
+}
+
+variable "ec2_sg_id" {
+  description = "ID of the EC2 security group"
+  type        = string
+}
+
+variable "target_group_arn" {
+  description = "ARN of the ALB target group"
+  type        = string
+}
+
+variable "ecr_repo_url" {
+  description = "URL of the ECR repository"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Docker image tag"
+  type        = string
+}
+
+variable "secret_arn" {
+  description = "ARN of the Secrets Manager secret"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "asg_min_size" {
+  description = "Minimum size of the Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_max_size" {
+  description = "Maximum size of the Auto Scaling Group"
+  type        = number
+}
+
+variable "asg_desired_capacity" {
+  description = "Desired capacity of the Auto Scaling Group"
+  type        = number
+}
+
+variable "aws_account_id" {
+  description = "AWS Account ID used in constructing resource ARNs"
+  type        = string
+}
+variable "environment" {
+  description = "Deployment environment (e.g., dev, stage, prod)"
+  type        = string
+}
+
