044ef37d2bd1dcb8284d9d35ddf9caae2aae615d
tharundej <95700240+tharundej@users.noreply.github.com>
2025-07-21 23:20:04 +0530

    Update backend.tf
    bucket updated

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-32co-20250719"  # Replace with your actual unique name
+    bucket = "terraform-bucket-test32"  # Replace with your actual unique name
     key    = "terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
   }
 }
