9b69266f234379669e462f593a17be3a7e1fa3bd
tharundej <95700240+tharundej@users.noreply.github.com>
2025-07-22 15:36:05 +0530

    Update main.tf

--- infrastructure/modules/ec2/main.tf
+++ infrastructure/modules/ec2/main.tf
@@ -62,7 +62,7 @@ resource "aws_launch_template" "app" {
   user_data              = base64encode(templatefile("${path.module}/../../user_data.sh", {
     ecr_repo_url = var.ecr_repo_url,
     image_tag    = var.image_tag,
-    secrets_arn   = var.secret_arn,
+    secret_arn   = var.secret_arn,
     aws_region   = var.aws_region,
     app_name     = var.app_name,
     rds_endpoint = aws_db_instance.main.endpoint
